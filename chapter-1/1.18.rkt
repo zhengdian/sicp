f23e60d168e2dfdf28444436c9c58fe78dd29223
zhengdian <1551070530@qq.com>
2017-07-03 23:31:38 +0800

    1.18

--- chapter-1/1.18.rkt
+++ chapter-1/1.18.rkt
@@ -12,4 +12,13 @@
         ((even? b) (double (fast-multi a (halve b))))
         (true (+ a (fast-multi a (- b 1))))))
 
-(fast-multi 8 29)
+(define (fast-multi-iter a b)
+  (define (f-m-i a b c)
+    (cond ((= b 0) 0)
+          ((= b 1) (+ a c))
+          ((even? b) (f-m-i (double a) (halve b) c))
+          (true (f-m-i a (- b 1) (+ a c)))))
+  (f-m-i a b 0))
+
+(fast-multi 8 39)
+(fast-multi-iter 8 39)
