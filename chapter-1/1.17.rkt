03ef788507c521a9d821462c1ecdaea2d1351983
zhengdian <1551070530@qq.com>
2017-07-03 23:06:16 +0800

    1.17

--- chapter-1/1.17.rkt
+++ chapter-1/1.17.rkt
@@ -1,1 +1,15 @@
 #lang planet neil/sicp
+
+(define (double x)
+  (* 2 x))
+
+(define (halve x)
+  (/ x 2))
+
+(define (fast-multi a b)
+  (cond ((= b 0) 0)
+        ((= b 1) a)
+        ((even? b) (double (fast-multi a (halve b))))
+        (true (+ a (fast-multi a (- b 1))))))
+
+(fast-multi 8 29)
