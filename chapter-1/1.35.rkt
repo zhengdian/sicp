1c85e0e12c4d798e7412a9ee807cf764c2591195
zhengdian <zdhust@foxmail.com>
2017-07-26 21:19:13 +0800

    1.35

---
+++ chapter-1/1.35.rkt
@@ -0,0 +1,19 @@
+#lang planet neil/sicp
+(define tolerance 0.000001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+;; gold^2 = gold + 1 => gold = 1 + 1/gold
+(define (gold)
+  (fixed-point (lambda (x) (+ 1 (/ 1 x)))
+               1.0))
+
+(gold)
