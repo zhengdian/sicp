e05c1940a8213f5285fca7d47421d57f545f7011
zhengdian <1551070530@qq.com>
2017-08-27 23:27:43 +0800

    2.5

---
+++ chapter-2/2.4.rkt
@@ -0,0 +1,13 @@
+#lang planet neil/sicp
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+(car (cons 4 1))
+(cdr (cons 4 56))
