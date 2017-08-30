42b11bdee11ef351744641098b4275a4d7bc7394
zhengdian <1551070530@qq.com>
2017-08-31 00:21:14 +0800

    2.6

--- chapter-2/2.6.rkt
+++ chapter-2/2.6.rkt
@@ -10,3 +10,30 @@
 
 (define two
   (lambda (f) (lambda (x) (f (f x)))))
+
+(define (plus first second)
+    (lambda (f)
+        (lambda (x)
+             ((first f) ((second f) x)))))
+
+(define (f x)
+    (display "*")
+    )
+
+
+(define (start-test-2-6)
+  
+  (display "going to display 1:")(newline)
+  ((one f) 'a)(newline)
+  (display "going to display 2:")(newline)
+  ((two f) 'a) (newline)
+  (display "going to display 1+2:")(newline)
+  (((plus one two) f) 'a)
+  (newline)
+  (display "going to display 1+2+2")(newline)
+  (((plus (plus one two) two) f) 'a)
+  (newline)
+
+  (display "end.") (newline))
+
+(start-test-2-6)
