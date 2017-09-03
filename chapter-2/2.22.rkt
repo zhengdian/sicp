cec88ae4fc2268f1d4434d03e9d6aea6b9efe400
zhengdian <1551070530@qq.com>
2017-09-03 15:19:30 +0800

    2.22

---
+++ chapter-2/2.22.rkt
@@ -0,0 +1,26 @@
+#lang planet neil/sicp
+(define (square x)
+  (* x x))
+
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) (append (cdr list1) list2))))
+
+(define (square-list items)
+  (if (null? items)
+      nil
+      (cons (square (car items)) (square-list (cdr items)))))
+
+;change cons to append
+(define (square-list-iter items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (append answer
+                      (list (square (car things)))))))
+  (iter items nil))
+
+(square-list (list 1 2 3 4 5 6 7))
+(square-list-iter (list 1 2 3 4 5 6 7))
