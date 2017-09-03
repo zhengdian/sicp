1a58e4d69bc1acc240e0cdb3b45b0065fa820960
zhengdian <1551070530@qq.com>
2017-09-03 15:46:27 +0800

    2.23

--- chapter-2/2.23.rkt
+++ chapter-2/2.23.rkt
@@ -1,1 +1,17 @@
 #lang planet neil/sicp
+
+(define (my-for-each f items)
+  (define (iter things answer)
+    (if (null? things)
+        true
+        (iter (cdr things) (f (car things)))))
+  (iter items nil))
+
+(for-each (lambda (x) (newline) (display x))
+          (list 1 2 3 4))
+
+(newline)
+(display "my-for-each: ")
+(my-for-each (lambda (x) (newline) (display x))
+          (list 1 2 3 4))
+
