ebee0cf2edc241bcd3a5692a8bd3d11d76de09d9
zhengdian <1551070530@qq.com>
2017-09-04 23:55:29 +0800

    2.28

---
+++ chapter-2/2.28.rkt
@@ -0,0 +1,12 @@
+#lang planet neil/sicp
+(define x (list (list 1 2) (list 3 4)))
+
+(display x)
+
+(define (fringe x)
+  (cond ((null? x) nil)
+        ((not (pair? (car x))) (append (list (car x)) (fringe (cdr x))))
+        (else (append (fringe (car x)) (fringe (cdr x))))))
+
+(display (fringe x))
+(display (fringe (list x x)))
